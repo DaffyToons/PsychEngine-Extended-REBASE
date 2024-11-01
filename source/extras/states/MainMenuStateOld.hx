b98a498a6b04620eda482be70dd1aea982b8b19e
The Man Daffyo Himself <87585374+DaffyToons@users.noreply.github.com>
2024-11-01 11:07:41 -0300

    Update MainMenuStateOld.hx

--- source/extras/states/MainMenuStateOld.hx
+++ source/extras/states/MainMenuStateOld.hx
@@ -69,7 +69,6 @@ class MainMenuStateOld extends MusicBeatState
 		#if MODS_ALLOWED
 		Paths.pushGlobalMods();
 		#end
-		WeekData.loadTheFirstEnabledMod();
 
 		#if desktop
 		// Updating Discord Rich Presence
